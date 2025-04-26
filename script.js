6ff97f0673637ea2ae31f6a70ceddb1fc3862c3c
LokeshLoky22 <lokeshbajoju08@gmail.com>
2025-04-26 16:36:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Auto sliding images
+let slides = document.querySelectorAll(".slide");
+let current = 0;
+
+function nextSlide() {
+  slides[current].classList.remove("active");
+  current = (current + 1) % slides.length;
+  slides[current].classList.add("active");
+}
+
+setInterval(nextSlide, 2000);
